24b5b6217380cbcf49baacd0b4e7a4eb90be78b0
Owen Williams <owilliams@mixxx.org>
2019-03-26 21:21:31 -0400

    Revert "update email for funded artists mentioning payment methods"
    This reverts commit 671089a60ee1812fa2cfbae26cef42248edcefb7.

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def account_activation
-    artist = default_artist
     if artist.activation_token.nil?
       artist.activation_token = ApplicationController.new_token
     end
@@ -10,7 +9,6 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   def password_reset
-    artist = default_artist
     if artist.reset_token.nil?
       artist.reset_token = ApplicationController.new_token
     end
@@ -19,48 +17,36 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   def voter_verified
-    voter = default_voter
-    UserMailer.voter_verified(voter, Rails.configuration.event_year)
+    UserMailer.voter_verified(voter, '2017')
   end
 
   def grant_funded
-    grant_submission = GrantSubmission
-      .where(funding_decision: true)
-      .where("granted_funding_dollars > 0")
-      .first || FactoryGirl.create(:grant_submission)
-    artist = Artist.where(id: grant_submission.artist).first || FactoryGirl.create(:artist)
-    grant = Grant.where(id: grant_submission.grant).first || FactoryGirl.create(:grant)
-    UserMailer.grant_funded(grant_submission, artist, grant, Rails.configuration.event_year)
+    UserMailer.grant_funded(grant_submission, artist, grant, '2017')
   end
 
   def grant_not_funded
-    grant_submission = GrantSubmission
-      .where(funding_decision: true)
-      .where("granted_funding_dollars == 0")
-      .first || FactoryGirl.create(:grant_submission)
-    artist ||= Artist.where(id: grant_submission.artist).take || FactoryGirl.create(:artist)
-    grant ||= Grant.where(id: grant_submission.grant).take || FactoryGirl.create(:grant)
-    UserMailer.grant_not_funded(grant_submission, artist, grant, Rails.configuration.event_year)
+    UserMailer.grant_not_funded(grant_submission, artist, grant, '2017')
   end
 
   def notify_questions
-    grant_submission = GrantSubmission.first || FactoryGirl.create(:grant_submission)
-    artist ||= Artist.where(id: grant_submission.artist).take || FactoryGirl.create(:artist)
-    grant ||= Grant.where(id: grant_submission.grant).take || FactoryGirl.create(:grant)
-    UserMailer.notify_questions(grant_submission, artist, grant, Rails.configuration.event_year)
+    UserMailer.notify_questions(grant_submission, artist, grant, '2017')
   end
 
   private
 
-  def default_artist
-    Artist.first || FactoryGirl.create(:artist, :activated)
+  def artist
+    @artist ||= Artist.first || FactoryGirl.create(:artist, :activated)
   end
 
-  def default_voter
-    Voter.first || FactoryGirl.create(:voter, :activated)
+  def voter
+    @voter ||= Voter.first || FactoryGirl.create(:voter, :activated)
   end
 
-  def default_grant
-    Grant.first || FactoryGirl.create(:grant)
+  def grant_submission
+    @grant_submission ||= GrantSubmission.first || FactoryGirl.create(:grant_submission)
+  end
+
+  def grant
+    @grant ||= Grant.first || FactoryGirl.create(:grant)
   end
 end
